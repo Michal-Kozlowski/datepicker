09f5c3b364a7dc50789868bc56ceaca562ad455b
MichalKozlowski <michalandrzejkozlowski@gmail.com>
2021-10-24 11:42:31 +0200

    [D0] - Initial commit

---
+++ src/components/AppDatepicker.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="datepicker">
+    This will be the datepicker component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppDatepicker',
+};
+</script>
+
+<style scoped lang="scss">
+.datepicker {
+
+}
+</style>
---
+++ src/components/AppReservation.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="reservation">
+    <button @click="toggleDatepicker">Toggle datepicker</button>
+    <AppDatepicker v-if="datepickerOpen" />
+  </div>
+</template>
+
+<script>
+import AppDatepicker from '@/components/AppDatepicker.vue';
+
+export default {
+  name: 'AppReservation',
+  components: {
+    AppDatepicker,
+  },
+  data() {
+    return {
+      datepickerOpen: false,
+    };
+  },
+  methods: {
+    toggleDatepicker() {
+      this.datepickerOpen = !this.datepickerOpen;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.reservation {
+
+}
+</style>
