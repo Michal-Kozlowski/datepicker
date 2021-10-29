175a66f91168a3c0e6bb2178b60f047d0f6d0c42
MichalKozlowski <michalandrzejkozlowski@gmail.com>
2021-10-29 22:07:16 +0200

    [D2] - Reservation changes

--- src/components/AppReservation.vue
+++ src/components/AppReservation.vue
@@ -1,30 +1,54 @@
 <template>
-  <div class="reservation">
+  <div :class="['reservation', { 'reservation--sent': reservationSent }]">
     <p class="reservation__price">{{ price }} zł</p>
     <div class="reservation__rate">
       <AppStars :rate="rate" />
       <p class="reservation__rate-count">{{ rateCount }}</p>
     </div>
-    <p class="reservation__dates-header">Dates</p>
+    <p class="reservation__header">Dates</p>
     <div class="reservation__datepicker" @click="toggleDatepicker">
-      <p class="reservation__check">Check In</p>
-      <p class="reservation__check">Check Out</p>
+      <p class="reservation__check">{{ dates.from || 'Check In' }}</p>
+      <p class="reservation__check">{{ dates.to || 'Check Out' }}</p>
     </div>
     <AppDatepicker v-if="datepickerOpen" />
-    <p class="reservation__selected-dates">17.12.2021 - 22.12.2021</p>
-    <div class="reservation__group-size">
+    <p class="reservation__header">Personal Info</p>
+    <form class="reservation__section">
+      <input
+        class="reservation__input reservation__item"
+        v-model="name"
+        type="text"
+        placeholder="First Name"
+      />
+      <input
+        class="reservation__input reservation__item"
+        v-model="surname"
+        type="text"
+        placeholder="Last Name"
+      />
+      <input
+        class="reservation__input reservation__item reservation__item--full"
+        v-model="email"
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        class="reservation__input reservation__item reservation__item--full"
+        v-model="phone"
+        type="phone"
+        placeholder="Phone"
+      />
+    </form>
+    <div class="reservation__actions reservation__section">
       <button
-        v-for="(number, index) in 5"
-        :key="`group-of-${index + 1}`"
-        class="reservation__button"
-      >
-        {{ number }}
+        @click="submitForm"
+        class="reservation__button reservation__accept reservation__item">
+        Accept
       </button>
-    </div>
-    <button class="reservation__button">Dog</button>
-    <div class="reservation__actions">
-      <button class="reservation__accept">Accept</button>
-      <button class="reservation__clear">Clear</button>
+      <button
+        @click="clearForm"
+        class="reservation__button reservation__clear reservation__item">
+        Clear
+      </button>
     </div>
   </div>
 </template>
@@ -56,11 +80,29 @@ export default {
   data() {
     return {
       datepickerOpen: false,
+      name: '',
+      surname: '',
+      email: '',
+      phone: '',
+      dates: {
+        from: '21-12-2021',
+        to: null,
+      },
+      reservationSent: false,
     };
   },
   methods: {
     toggleDatepicker() {
       this.datepickerOpen = !this.datepickerOpen;
+    },
+    submitForm() {
+      this.reservationSent = true;
+    },
+    clearForm() {
+      this.name = '';
+      this.surname = '';
+      this.email = '';
+      this.phone = '';
     },
   },
 };
@@ -68,11 +110,42 @@ export default {
 
 <style scoped lang="scss">
 .reservation {
+  font-size: 18px;
+  position: relative;
   width: 360px;
   padding: $s24 $s32;
   margin: $s8;
   border: 1px solid $borderGrey;
   background-color: $backgroundGrey;
+
+  &--sent {
+    &::before,
+    &::after {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      left: 0;
+    }
+
+    &::before {
+      content: '';
+      top: 0;
+      z-index: 2;
+      background-color: rgba($black, 0.4);
+      backdrop-filter: blur(5px);
+    }
+
+    &::after {
+      content: 'Thank you for booking :)';
+      font-size: 24px;
+      position: absolute;
+      top: calc(50% - $s24/2);
+      text-align: center;
+      z-index: 3;
+      color: $washedGreen;
+      text-shadow: 0 0 16px $black;
+    }
+  }
 
   &__price {
     font-size: 28px;
@@ -84,7 +157,6 @@ export default {
     display: flex;
     width: 100%;
     padding-bottom: $s24;
-    margin-bottom: $s24;
     border-bottom: 1px solid $borderGrey;
   }
 
@@ -95,20 +167,21 @@ export default {
     margin-left: $s8;
   }
 
-  &__dates-header {
+  &__header {
     font-size: 14px;
     font-weight: 700;
+    margin-top: $s24;
     margin-bottom: $s8;
   }
 
   &__datepicker {
+    position: relative;
     display: flex;
     justify-content: space-between;
     padding: $s8;
     border: 1px solid $borderGrey;
-    border-radius: $s2;
+    border-radius: $s4;
     cursor: pointer;
-    position: relative;
     transition-duration: $transition-duration;
 
     &:hover {
@@ -117,9 +190,9 @@ export default {
 
     &::before {
       content: "";
+      position: absolute;
       width: 24px;
       height: 2px;
-      position: absolute;
       top: calc(50% - 2px / 2);
       left: calc(50% - 24px / 2);
       background-color: $mainTextGrey;
@@ -127,9 +200,9 @@ export default {
 
     &::after {
       content: "";
+      position: absolute;
       width: 10px;
       height: 10px;
-      position: absolute;
       top: calc(50% - 12px / 2);
       left: calc(50% + 2px / 2);
       border-top: 2px solid $mainTextGrey;
@@ -139,10 +212,9 @@ export default {
   }
 
   &__check {
-    font-size: 18px;
-    border-radius: $s4;
-    padding: $s4 $s8;
     width: calc(50% - 30px - $s16);
+    padding: $s4 $s8;
+    border-radius: $s4;
 
     &--active {
       background-color: $washedAccentGreen;
@@ -150,24 +222,60 @@ export default {
   }
 
   &__selected-dates {
+    padding: $s8;
+    margin-top: $s16;
+    border: 1px solid $borderGrey;
+    text-align: center;
   }
 
-  &__group-size {
+  &__section {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    border-radius: $s4;
+  }
+
+  &__item {
+    width: calc(50% - 3 * $s8);
+    padding: $s12 $s8;
+    margin-bottom: $s16;
+    font-size: inherit;
+    border: 1px solid $borderGrey;
+
+    &--full {
+      width: 100%;
+    }
+  }
+
+  &__input {
+    background-color: transparent;
+    border-radius: $s4;
   }
 
   &__button {
+    border-radius: $s4;
+    transition-duration: $transition-duration;
+    cursor: pointer;
+
+    &:hover {
+      color: $white;
+    }
   }
 
   &__actions {
-
+    margin-top: $s16;
   }
 
   &__accept {
-
+    &:hover {
+      background: $mainGreen;
+    }
   }
 
   &__clear {
-
+    &:hover {
+      background: $mainRed;
+    }
   }
 }
 </style>
